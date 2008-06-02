e076a81fdcc5c5e6454ca9943394ab719161e93e
jim <jim@4e22fdd3-d838-0410-b171-8f2b50891703>
2008-06-02 20:45:59 +0000

    added some attribute validations
    git-svn-id: https://svn.office.gdi/development/travis/trunk@2630 4e22fdd3-d838-0410-b171-8f2b50891703

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ActiveRecord::Base
   has_and_belongs_to_many :stories
+  validates_presence_of :description, :title
 end
--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class TaskTest < ActiveSupport::TestCase
   def test_find_all_tasks
@@ -15,7 +15,17 @@ class TaskTest < ActiveSupport::TestCase
   end
 
   def test_story_relation
-    t = Task.new
+    t = Task.new :title=>"New Task Title", :description=>"New Task Description"
     assert_not_nil(t.stories)
+    assert(t.save)
+    t.stories.create({:title=>"Story Title"})
+    assert_equal(Story.find_by_title("Story Title"), t.stories.first())
+  end
+
+  def test_valid_atttributes
+    t = Task.new 
+    assert(!t.save)
+    assert_equal t.errors.on(:description), "can't be blank"
+    assert_equal t.errors.on(:title), "can't be blank"
   end
 end

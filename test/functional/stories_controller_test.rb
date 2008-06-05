256a5161eaef8a881998034f1f917f6d918329d1
mcclain <mcclain@4e22fdd3-d838-0410-b171-8f2b50891703>
2008-06-05 18:01:54 +0000

    converted to new assert_routing
    git-svn-id: https://svn.office.gdi/development/travis/trunk@2681 4e22fdd3-d838-0410-b171-8f2b50891703

--- test/functional/stories_controller_test.rb
+++ test/functional/stories_controller_test.rb
@@ -6,17 +6,11 @@ class StoriesControllerTest < ActionController::TestCase
   end
   def test_routes
     assert_routing "/", :controller=>"stories",:action=>"index"
-
     assert_routing "/stories/new", :controller=>"stories",:action=>"new"
-
     assert_routing "/stories/1", :controller=>"stories",:action=>"show", :id=>"1"
-
-    assert_recognizes({:controller=>"stories",:action=>"create"}, :path=>"/stories", :method=>"post")
-
-    assert_recognizes({:controller=>"stories",:action=>"destroy", :id=>"1"}, :path=>"/stories/1", :method=>"delete")
-
-    assert_recognizes({:controller=>"stories",:action=>"update", :id=>"1"}, :path=>"/stories/1", :method=>"put")
-
+    assert_routing({:path=>'/stories', :method=>'post'}, {:controller=>"stories",:action=>"create"})
+    assert_routing({:path=>'/stories/1', :method=>'delete'}, {:controller=>"stories",:action=>"destroy", :id=>'1'})
+    assert_routing({:path=>'/stories/1', :method=>'put'}, {:controller=>"stories",:action=>"update", :id=>'1'})
     assert_routing "/stories/1/edit", :controller=>"stories",:action=>"edit", :id=>"1"
   end
 

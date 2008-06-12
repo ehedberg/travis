161ef437c7fccb27bc13969c5603dd49ff7bcffc
mcclain <mcclain@4e22fdd3-d838-0410-b171-8f2b50891703>
2008-06-12 17:18:05 +0000

    git-svn-id: https://svn.office.gdi/development/travis/trunk@2807 4e22fdd3-d838-0410-b171-8f2b50891703

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,12 @@ class DashboardController < ApplicationController
   def index
     t = Date.today
     @iteration = Iteration.find(:first, :conditions=>['start_date <= ? and end_date >= ?', t,t])
+    if @iteration && Iteration.count  > 0
+      all_iterations = Iteration.find(:all, :order=>'start_date asc')
+      @next_iteration= all_iterations[all_iterations.index(@iteration)+1]
+      @prev_iteration= all_iterations[all_iterations.index(@iteration)-1]
+    else
+      render :template=>'dashboard/empty'
+    end
   end
 end
--- test/functional/dashboard_controller_test.rb
+++ test/functional/dashboard_controller_test.rb
@@ -11,8 +11,17 @@ class DashboardControllerTest < ActionController::TestCase
     assert i.start_date <= Date.today
     assert i.end_date >= Date.today
     assert_equal i, iterations(:current)
+    assert assigns(:next_iteration)
+    assert assigns(:prev_iteration)
   end
   def test_routin
     assert_routing("/", :controller=>'dashboard', :action=>'index')
+  end
+  def test_works_w_no_iterations
+    Iteration.destroy_all
+    assert_equal 0, Iteration.count
+    get :index
+    assert_response:success
+    assert_template 'empty'
   end
 end

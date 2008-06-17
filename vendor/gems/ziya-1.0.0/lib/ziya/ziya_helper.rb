33e18af4996995aabc51329b5476bfb0d560fee4
mcclain <mcclain@4e22fdd3-d838-0410-b171-8f2b50891703>
2008-06-17 19:36:54 +0000

    git-svn-id: https://svn.office.gdi/development/travis/trunk@2902 4e22fdd3-d838-0410-b171-8f2b50891703

--- vendor/gems/ziya-1.0.0/lib/ziya/ziya_helper.rb
+++ vendor/gems/ziya-1.0.0/lib/ziya/ziya_helper.rb
@@ -86,7 +86,7 @@ module Ziya
 
        # Const accessors...
        def chart_path() 
-       root_path != '/' ? root_path+"/charts" : root_path 
+        "/charts"
        end       
        def class_id()   "clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" end
        def codebase()   "http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"; end

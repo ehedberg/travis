f7e0525a8beeb90443796e3849e28b02c8e15ed6
mcclain <mcclain@4e22fdd3-d838-0410-b171-8f2b50891703>
2008-06-19 20:42:22 +0000

    git-svn-id: https://svn.office.gdi/development/travis/trunk@2955 4e22fdd3-d838-0410-b171-8f2b50891703

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery  :secret => 'e763e694b24cbffe0c373f235e368b04'
+#  protect_from_forgery  :secret => 'e763e694b24cbffe0c373f235e368b04'
   private
   def requires_login
     if session[:login]

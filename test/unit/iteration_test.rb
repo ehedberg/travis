57fc59c073d82c9fa5e6d399a3ef1465054df6b5
mcclain <mcclain@4e22fdd3-d838-0410-b171-8f2b50891703>
2008-07-14 15:01:57 +0000

    woopsie!
    git-svn-id: https://svn.office.gdi/development/travis/trunk@3278 4e22fdd3-d838-0410-b171-8f2b50891703

--- test/unit/iteration_test.rb
+++ test/unit/iteration_test.rb
@@ -73,5 +73,11 @@ class IterationTest < ActiveSupport::TestCase
   def test_previous_iter_has_no_previous
       foo = iterations(:last)
       assert_nil foo.previous
-    end
+  end
+
+  def test_has_many_releases
+    i = iterations(:many_releases)
+    assert_equal 2, i.stories.length
+  end
+
 end

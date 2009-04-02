b69ff8962147c87c34c0558b4add05b5ff4c5ac0
Eric Hedberg <eric.hedberg@govdelivery.com>
2009-04-02 15:42:45 -0500

    production setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,10 +11,10 @@ config.cache_classes = false
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
-config.action_view.cache_template_loading            = false
+
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = false
